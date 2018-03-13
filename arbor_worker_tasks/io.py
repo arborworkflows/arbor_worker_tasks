2d33d9039e617f379800d4d3e07971c375537eb8
Jeffrey Baumes <jeff.baumes@kitware.com>
2018-03-13 16:44:23 -0400

    Add appendColumns task

---
+++ arbor_worker_tasks/io.py
@@ -0,0 +1,15 @@
+
+def fileToRows(filename):
+    import pandas as pd
+
+    df = pd.read_csv(filename)
+    return {
+        'fields': df.columns,
+        'rows': [{k: v for k, v in row.iteritems()} for _, row in df.iterrows()]
+    }
+
+def rowsToFile(table):
+    import pandas as pd
+
+    df = pd.DataFrame(table['rows'], columns=table['fields'])
+    df.to_csv('out.csv', index=False)
---
+++ test.py
@@ -0,0 +1,4 @@
+from arbor_worker_tasks.tasks import appendColumns
+
+
+f = appendColumns('in1.csv', 'in2.csv', 'id')
